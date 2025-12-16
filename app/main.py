1043e43a9774f6602f114986974f0bc6a7a87515
Sandeep Pucha <sandeeppucha204@gmail.com>
2025-12-16 07:28:55 +0530

    Matplotlib and Seaborn

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 dataFrame = pd.read_csv("data/Sample.csv")
+print(dataFrame.head())
+dataFrame = pd.DataFrame(dataFrame)
 
-dataFrame['New Global Revenue'] = dataFrame['Global Revenue'] + 2000
-print(dataFrame['New Global Revenue'])
+# Bar Plot using Matplotlib
+plt.bar(dataFrame['Date'], dataFrame['Global Revenue'])
+plt.show()
+
+# Bar Plot using Seaborn
+dataFrame = sns.load_dataset("iris")
+print(dataFrame.species.value_counts().plot(kind="barh"))
+
+plt.title("Species Count", fontsize=16, fontweight='bold', color='Green')
+plt.xlabel("Count")
+plt.ylabel("Species")
+
+plt.show() 
